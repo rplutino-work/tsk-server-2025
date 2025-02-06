2116709e08ac101e1f3cc64cc395b8f5d66e4a84
rplutino-work <rodrigoivanplutino@gmail.com>
2025-02-06 00:35:21 -0300

    first commit

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -12,5 +13,8 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  admin: {
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+  },
 })
